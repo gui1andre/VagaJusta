c737e70d6b68a02db62ac190e62d1596f539e099
Guilherme Andre <gui1andre89@gmail.com>
2026-06-01 16:50:09 -0300

    criando api e DependencyInjection de Infra e Application

---
+++ VagaJusta.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VagaJusta.Application.Behaviors;
+
+namespace VagaJusta.Application
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services.AddMediatR(cfg =>
+                {
+                    cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+                    cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+                });
+        }
+    }
+}
---
+++ VagaJusta.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VagaJusta.Domain.Interfaces.Repositories;
+using VagaJusta.Infrastructure.Data;
+using VagaJusta.Infrastructure.Data.Repositories;
+
+namespace VagaJusta.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DBContext>(op => op.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IAlunoRepository, AlunoRepository>();
+            services.AddScoped<IEscolaRepository, EscolaRepository>();
+            services.AddScoped<ITurmaRepository, TurmaRepository>();
+
+            return services;
+        }
+    }
+}
