fa0118458b1a104fdd81a0c8449c09dcc5a21667
Guilherme Andre <gui1andre89@gmail.com>
2026-05-31 22:32:18 -0300

    criacao dbcontext e instalacao packages entity

---
+++ VagaJusta.Infrastructure/Data/DBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VagaJusta.Domain.Entities;
+
+namespace VagaJusta.Infrastructure.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Escola> Escolas => Set<Escola>();
+        public DbSet<Aluno> Alunos => Set<Aluno>();
+        public DbSet<Turma> Turma => Set<Turma>();
+        public DbSet<Matricula> Matriculas => Set<Matricula>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DBContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
